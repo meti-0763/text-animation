74c19a37faedc9c3e914a5b634abf43c576736d5
meti-0763 <daramade4@gmail.com>
2025-01-09 00:31:06 +0330

    end

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let myText  = `design by meti :`
 
-
+   const text = "design by meti :)";
+   const speed = 100; // سرعت تایپ (میلی‌ثانیه)
+   let index = 0;
+ 
+   function typeEffect() {
+     if (index < text.length) {
+       document.getElementById("text").textContent += text[index];
+       index++;
+       setTimeout(typeEffect, 100);
+     }
+   }
+   
+   typeEffect()
